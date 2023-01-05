bd528c5d0374ac4b2bd03686fecd0ffe8ddaaf40
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-05 19:42:11 +0900

    지연 로딩, 연관관계 메서드

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,19 +6,21 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
-public class orderItem {
+public class OrderItem {
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
