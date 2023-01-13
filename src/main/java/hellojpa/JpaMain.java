373f96cd57824edca2c3aee788ee51bc170f8d8c
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-13 21:01:19 +0900

    Jpa crud

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,12 +1,52 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // create
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+
+//            Member findMember = em.find(Member.class, 1L);
+
+            // delete
+//            em.remove(findMember);
+
+            // update
+            // 값을 변경하면 commit 시점에 변경을 감지하여 update 됨.
+//            findMember.setName("HelloJpa");
+
+            // read
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 }
