2ac90cc9283c12b86611ded3aee9a48bae134653
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-18 15:16:17 +0900

    다대일

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -37,21 +37,4 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
-
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-
-    @Override
-    public String toString() {
-        return "Team{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", members=" + members +
-                '}';
-    }
 }
