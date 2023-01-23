6049e54b56e0eafbc0709affa8b90a5b55b1a1aa
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-23 18:52:21 +0900

    orphanRemoval

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,6 +29,13 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+//            em.remove(findParent);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
