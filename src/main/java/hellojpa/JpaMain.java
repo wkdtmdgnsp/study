b7001efc1f9c8dde543d4031b252815f89c3033d
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-23 18:36:20 +0900

    caseCade

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,43 +20,14 @@ public class JpaMain {
 
         try {
 
-            Team teamA = new Team();
-            teamA.setName("teamA");
-            em.persist(teamA);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(teamA);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member1");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            /**
-             * fetch LAZY, EAGER
-             */
-//            Member m = em.find(Member.class, member1.getId());
-//
-//            System.out.println("m = " + m.getTeam().getClass());
-//
-//            System.out.println("==============");
-//            System.out.println("m.getTeam().getName() = " + m.getTeam().getName());
-//            System.out.println("==============");
-
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
-
-            // SQL : select * from Member
-            // SQL : select * from Member where TEAM_ID = xxx
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
