20d5e0b8d3ecd1c8bf8164a5b0bd2789e1cfd1ce
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-23 20:12:47 +0900

    embedded type

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,29 +5,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-// @SequenceGenerator 시퀀스 정보 직접 지정
-// @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-// @TableGenerator 테이블을 활용해 시퀀스 처럼 사용한다. 성능은 떨어지지만 모든 DB에서 적용 가능, 잘안씀
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
-    // @Id 직접 할당
-    // @GeneratedValue 자동 할당
-    /**
-     *  IDENTITY 기본 키 생성을 데이터베이스에 위임
-     *  sql 실행해야 기본키를 알수 있기 떄문에 persist 하면 바로 쿼리 생성하여 DB에 날림
-     *  그래서 임시 저장소에 모은뒤 커밋 시점에 sql 실행을 하지 않음
-     */
-
-    /**
-     *      SEQUENCE 시퀀스를 만들어 사용
-     *      allocationSize = 1 일때,
-     *      persist 할떄마다 DB 시퀀스 값을 가져옴. 쿼리는 임시 저장소에 저장하고 커밋시점에 실행
-     *      allocationSize = 50 (기본값) 일때,
-     *      처음 persist 할때 DB 시퀀스에서 50개를 가져와 놓고,
-     *      이후 메모리에서 50개 사용후 다시 DB 시퀀스 값을 가져옴 (성능 최적화)
-     *      여러대의 서버에서도 동시성 문제 없음
-     */
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
@@ -35,15 +15,24 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    /**
-     * LAZY 프록시로 조회
-     * 지연 로딩 (실제 사용할때 초기화) 쿼리 여러번
-     * EAGER 즉시 로딩
-     * 조인으로 한번에 같이 조회
-     */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
-    private Team team;
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
+
+    // 주소
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city",
+                    column=@Column(name = "WORK_CITY")),
+            @AttributeOverride(name="street",
+                    column=@Column(name = "WORK_STREET")),
+            @AttributeOverride(name="zipcode",
+                    column=@Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -61,11 +50,19 @@ public class Member extends BaseEntity {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }
