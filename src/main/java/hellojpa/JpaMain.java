ab665a60fe9cfeca0c24020733192d060782e590
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-25 23:24:16 +0900

    jpql, Criteria, Native SQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -20,51 +23,52 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 저장
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            // jpql
+//            String qlString = "select m from Member m where m.username like '%kim'";
+//            List<Member> result = em.createQuery(
+//                    qlString,
+//                    Member.class
+//            ).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//            }
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-//            member.getAddressesHistory().add(new Address("old1", "street", "10000"));
-//            member.getAddressesHistory().add(new Address("old2", "street", "10000"));
-            member.getAddressesHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressesHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("================= START ===========");
-
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 조회
-//            List<Address> addressesHistory = findMember.getAddressesHistory();
-//            for (Address address : addressesHistory) {
-//                System.out.println("address.getCity() = " + address.getCity());
+            //Criteria
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//
+//            Root<Member> m = query.from(Member.class);
+//
+//            CriteriaQuery<Member> cq = query.select(m);
+//
+//            String username = "dsafas";
+//            if (username != null) {
+//                cq = cq.where(cb.equal(m.get("username"), "kim"));
 //            }
 //
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-//            for (String favoriteFood : favoriteFoods) {
-//                System.out.println("favoriteFood = " + favoriteFood);
+//            List<Member> resultList = em.createQuery(cq)
+//                    .getResultList();
+
+            // Native SQL
+
+//            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+//                    .getResultList();
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            // flush -< commit, query
+
+            em.flush();
+
+            // 결과 0
+            // dbconn.executeQuery("select * from member");
+
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
 //            }
-
-            // 수정
-            // homeCity -> newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressesHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressesHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
