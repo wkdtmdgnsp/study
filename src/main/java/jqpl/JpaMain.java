7302bdaf453cb6f364f16937a3b4535e575c01ee
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-26 00:03:14 +0900

    basic query api

---
+++ src/main/java/jqpl/JpaMain.java
@@ -0,0 +1,52 @@
+package jqpl;
+
+import javax.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+//            List<Member> resultList = query.getResultList();
+//
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+//            Member result = query.getSingleResult();
+//
+//            // Spring Data JPA ->
+//            System.out.println("result = " + result);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("result = " + result.getUsername());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
