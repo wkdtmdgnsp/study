c62874a521d48a5c41f796dd1b75ff1a39a6675f
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-26 23:50:23 +0900

    프로젝션

--- src/main/java/jqpl/JpaMain.java
+++ src/main/java/jqpl/JpaMain.java
@@ -1,6 +1,10 @@
 package jqpl;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,26 +22,57 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            em.flush();
+            em.clear();
 
-//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-//            List<Member> resultList = query.getResultList();
+            // 엔티티 프로젝션
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
 //
-//            for (Member member1 : resultList) {
-//                System.out.println("member1 = " + member1);
-//            }
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
+            
+            // 명시적 조인 
+//            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+//                    .getResultList();
 
-//            Member result = query.getSingleResult();
+            // 묵시적 조인
+//            List<Team> result = em.createQuery("select m.team from Member m", Team.class)
+//                    .getResultList();
+
+            // 임베디드 타입 프로젝션
+            // 임베디드 타입만으로는 안되고 그것의 엔티티로 시작
+//            em.createQuery("select o.address from Order o", Address.class)
+//                    .getResultList();
+
+            // 스칼라 타입 프로젝션
+//                em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
+
+            // Query 타입 조회
+//            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
 //
-//            // Spring Data JPA ->
-//            System.out.println("result = " + result);
+//            Object o = resultList.get(0);
+//            Object[] result = (Object[]) o;
+//            System.out.println("result[0] = " + result[0]);
+//            System.out.println("result[1] = " + result[1]);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            // Object[] 타입 조회
+//            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
+//
+//            Object[] result = resultList.get(0);
+//            System.out.println("result[0] = " + result[0]);
+//            System.out.println("result[1] = " + result[1]);
 
-            System.out.println("result = " + result.getUsername());
+            // new 명령어로 조회
+            List<MemberDTO> result = em.createQuery("select new jqpl.MemberDTO (m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
