079b21c6ebc53b22eb998f5717778d0030b385e4
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-01-30 14:13:21 +0900

    Bulk Operation

--- src/main/java/jqpl/JpaMain.java
+++ src/main/java/jqpl/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -40,16 +39,31 @@ public class JpaMain {
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            /**
+             * 벌크 연산
+             * 단건은 Dirty Checking 통해 UPDATE
+             * 여러건(대량)을 한 번에 수정하거나 삭제
+             * 영속성 컨텍스트를 무시하고 DB에 직접 쿼리
+             * 벌크 연산을 먼저 실행 수행후 컨텍스트를 초기화 해줘야함
+             * flush 자동 호출 commit, query
+             */
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+//            System.out.println("resultCount = " + resultCount);
+
+//            // 컨텍스트를 초기화 하지 않으면
+//            // 1차캐시에 있는 0 0 0 으로 표기 됨
+//            System.out.println("member1 = " + member1.getAge());
+//            System.out.println("member2 = " + member2.getAge());
+//            System.out.println("member3 = " + member3.getAge());
+
+            // 초기화후 다시 가져와야 제대로 나이 반영
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
