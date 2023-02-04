a246c4c1c1253c1a4912c70e75c2d949511805cb
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-02-04 19:50:52 +0900

    itemList

--- src/main/java/jpabook/jpashop2/controller/ItemController.java
+++ src/main/java/jpabook/jpashop2/controller/ItemController.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop2.controller;
 
 import jpabook.jpashop2.domain.item.Book;
+import jpabook.jpashop2.domain.item.Item;
 import jpabook.jpashop2.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,5 +35,12 @@ public class ItemController {
         itemService.saveItem(book);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
