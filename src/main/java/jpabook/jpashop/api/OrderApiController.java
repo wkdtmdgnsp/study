8235c19bc6febf4383f867653e83fc7208e12c55
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-02-11 19:17:16 +0900

    collection ordersV3, DTO fetch join 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -46,6 +46,16 @@ public class OrderApiController {
         return result;
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+
+        return result;
+    }
+
     @Getter
     static class OrderDto {
 
