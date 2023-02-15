2b0d41704d6473c9150d271905aca2f7aaec4e74
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2023-02-15 18:11:52 +0900

    spring converter apply

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +24,10 @@ public class HelloController {
         return "ok";
     }
 
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort Ip = " + ipPort.getIp());
+        System.out.println("ipPort Port = " + ipPort.getPort());
+        return "ok";
+    }
 }
